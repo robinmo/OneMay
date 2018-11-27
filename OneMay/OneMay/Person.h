a8c31d851aba8a6cf93d5dc82776e9ca4e2eb632
robin <robin@163.com>
2018-11-27 18:39:50 +0800

    创建了Person类

---
+++ OneMay/OneMay/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  OneMay
+//
+//  Created by Robin on 2018/11/27.
+//  Copyright © 2018 Robin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OneMay/OneMay/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  OneMay
+//
+//  Created by Robin on 2018/11/27.
+//  Copyright © 2018 Robin. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
